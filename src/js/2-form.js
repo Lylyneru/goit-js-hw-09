77d780f80d5546c026a387ba03d574a20febdec7
Lylyneru <lyly777@gmail.com>
2024-11-19 18:21:37 +0200

    css done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,62 +1,13 @@
-const divContainer = document.createElement('div');
-divContainer.classList.add('form-container');
+document.addEventListener('DOMContentLoaded', () => {
+  const divContainer = document.createElement('div');
+  divContainer.classList.add('form-container');
 
-const form = document.querySelector('.feedback-form');
-form.classList.add('form-class');
+  const form = document.querySelector('.feedback-form');
+  form.classList.add('feedback-form');
 
-divContainer.appendChild(form);
-document.body.appendChild(divContainer);
-
-const labels = form.querySelectorAll('label');
-labels.forEach(label => {
-  label.classList.add('form-label');
-
-  label.style.display = 'block';
-  label.style.marginBottom = '8px';
-  label.style.font = 'Montserrat';
-  label.style.weight = '400';
-  label.style.size = '16px';
-  label.style.lineHeight = '24px';
+  divContainer.appendChild(form);
+  document.body.appendChild(divContainer);
 });
-
-const inputs = form.querySelectorAll('input');
-inputs.forEach(input => {
-  input.classList.add('form-input');
-
-  input.style.width = '360px';
-  input.style.height = '40px';
-  input.style.marginBottom = '8px';
-});
-
-const text = form.querySelectorAll('textarea');
-textarea.forEach(textarea => {
-  textarea.classList.add('form-textarea');
-
-  textarea.style.width = '360px';
-  textarea.style.height = '80px';
-  textarea.style.marginBottom = '16px';
-});
-
-const button = form.querySelector('button');
-button.classList.add('form-button');
-
-button.style.padding = '8px 16px';
-button.style.backgroundColor = '#4E75FF';
-button.style.color = 'white';
-button.style.border = 'none';
-button.style.borderRadius = '8px';
-button.style.cursor = 'pointer';
-button.style.width = '95px';
-button.style.height = '40px';
-
-divContainer.style.maxWidth = '408px';
-divContainer.style.height = '296px';
-divContainer.style.gap = '16px';
-divContainer.style.margin = '0 auto';
-divContainer.style.padding = '24px';
-divContainer.style.border = '1px solid #ccc';
-divContainer.style.borderRadius = '8px';
-divContainer.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
 
 const formData = {
   email: '',
